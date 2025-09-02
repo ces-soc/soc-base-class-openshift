afe3675fd08fc97e933a86956c52a65bf237d682
Beth Perkins <80359562+goldenpilipala@users.noreply.github.com>
2025-09-02 16:27:39 -0600

    Update humio.py

--- cessoc/humio.py
+++ cessoc/humio.py
@@ -121,7 +121,7 @@ def create_session() -> requests.sessions.Session:
     retry_strategy = Retry(
         total=5,
         backoff_factor=5,
-        # backoff_max=30,
+        backoff_max=30,
         # These status codes indicate something temporarily wrong, fixable by re-request
         status_forcelist=[408, 429, 500, 502, 503, 504],
         allowed_methods=["GET", "POST"],
