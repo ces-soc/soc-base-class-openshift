2727f5b140141ee8880b37c88c410cec4288401d
Joseph McFadden <118224812+jm585@users.noreply.github.com>
2025-10-01 13:24:29 -0600

    Update __init__.py

--- cessoc/__init__.py
+++ cessoc/__init__.py
@@ -1,4 +1,4 @@
 """
 The cessoc package provides the main functionality used by the cessoc engineering team.
 """
-__all__ = ["healthcheck", "humio", "postgresql", "rabbitmq", "util", "timestamps", "etl", "configmanager"]
+__all__ = ["healthcheck", "humio", "postgresql", "rabbitmq", "util", "timestamps", "etl", "configmanager", "colors"]
